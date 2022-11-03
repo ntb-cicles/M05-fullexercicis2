2c2900138402962bbe87588896dec0eaf00f715a
Pofesso <Pofesso@DESKTOP-5FHJO2T>
2022-11-03 15:42:55 +0100

    Tercer commit

--- src/main/java/com/mycompany/conversio/Conversio.java
+++ src/main/java/com/mycompany/conversio/Conversio.java
@@ -16,7 +16,7 @@ public class Conversio {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        System.out.println("Introdueix en €");
+        System.out.println("Introdueix en € ");
         int valor_euros = sc.nextInt();
                 
         double tipus_de_canvi = 1.15d;
